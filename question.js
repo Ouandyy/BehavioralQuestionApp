50bb67c184725a4ae1a1c75f673053c3c8999de5
Andy Ou <ouandyy@gmail.com>
2022-06-08 22:58:36 -0700

    questions

---
+++ question.js
@@ -0,0 +1,10 @@
+First and foremost thank you for giving me some time to ask questions, I would like to hear your feed back and how I can improve myself.
+
+What would make a good candidate?
+
+What do you think I lacked that might affect my chances on moving to the next round?
+
+What stack do you guys use?
+
+When would I be hearing back from you guys?
+
